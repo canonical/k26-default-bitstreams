d8ef05571a039d5eeb503b4dd172374c0cbefc6e
Artie Poole <stuart.poole@canonical.com>
2025-07-09 16:32:26 +0100

    k26-default-bitstreams application is called on boot and loads k26-starter-kits.bit.bin to dev
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

---
+++ k26-default-bitstreams/src/proxies/mod.rs
@@ -0,0 +1,8 @@
+// This file is part of k26-default-bitstreams, an accompanying application to fpgad (https://github.com/canonical/fpgad)
+// Copyright 2025 Canonical Ltd.
+// SPDX-License-Identifier: GPL-3.0-only
+// k26-default-bitstreams is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3, as published by the Free Software Foundation.
+// k26-default-bitstreams is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranties of MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+
+pub mod control_proxy;
